b06a8d2763b9b6cbbc1fcd8b7a25ab52be205219
ViHoAT <vinh.ho-vn@hotmail.com>
2026-06-15 17:55:04 +0200

    Enemy Basic Movement Behavior
    Enemy can walk towards player without phasing through  walls, attack through walls are still possible

---
+++ Assets/Scripts/Player/PlayerWASDMovement.cs
@@ -0,0 +1,65 @@
+using System;
+
+using UnityEngine;
+
+using UnityEngine.InputSystem;
+
+
+
+public class PlayerMovement : MonoBehaviour
+
+{
+
+    [SerializeField] private float movementPerSecond;
+
+    [SerializeField] private float rotationPerSecond;
+
+    [SerializeField] private GameObject camera;
+
+
+    private void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.W))
+
+            transform.position += transform.forward * movementPerSecond * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A))
+
+            transform.position += -transform.right * movementPerSecond * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.S))
+
+            transform.position += -transform.forward * movementPerSecond * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.D))
+
+            transform.position += transform.right * movementPerSecond * Time.deltaTime;
+
+
+        if (Input.GetMouseButton(1))
+
+        {
+
+            camera.transform.RotateAround(transform.position, new Vector3(0, 1, 0), rotationPerSecond * Time.deltaTime);
+
+        }
+
+        if (Input.GetMouseButton(0))
+
+        {
+
+            camera.transform.RotateAround(transform.position, new Vector3(0, 1, 0), -rotationPerSecond * Time.deltaTime);
+
+        }
+
+    }
+
+
+}
