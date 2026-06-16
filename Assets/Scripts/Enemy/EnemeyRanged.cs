596855e71d39603a733946657fd5c36093bda3cd
ViHoAT <vinh.ho-vn@hotmail.com>
2026-06-16 19:07:43 +0200

    Enemy Attacks
    Implemented Attack sequence for ranged/flying melee/ground melee

--- Assets/Scripts/Enemy/EnemeyRanged.cs
+++ Assets/Scripts/Enemy/EnemeyRanged.cs
@@ -1,10 +1,17 @@
+using System.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemeyRanged : EnemyBase
 {
+    [SerializeField] private Transform firePoint;
+    [SerializeField] private Gradient chargeUpColors;
+    [SerializeField] private float chargeUpTime = 1.5f;
+    [SerializeField] private float laserDuration = .5f;
+    [SerializeField] private GameObject explosionPrefab;
     private LineRenderer _lineRenderer;
-    private Ray _ray;
+    private MeshRenderer _meshRenderer;
+    
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     protected override void Start()
@@ -12,13 +19,36 @@ public class EnemeyRanged : EnemyBase
         base.Start();
         _lineRenderer = GetComponent<LineRenderer>();
         _lineRenderer.enabled = false;
+        _meshRenderer = GetComponentInChildren<MeshRenderer>();
     }
 
     protected override void Attack()
     {
-        _lineRenderer.enabled = true;
-        _lineRenderer.SetPosition(0, transform.position);
-        _lineRenderer.SetPosition(1, player.transform.position);
-        WaitForSeconds wait = new WaitForSeconds(20f);
+        StartCoroutine(LaserSequence());
     }
+
+    private IEnumerator LaserSequence()
+    {
+        float timer = 0f;
+
+        while(timer < chargeUpTime)
+        {
+            timer += Time.deltaTime;
+
+            float chargeUpPercentage = timer / chargeUpTime;
+
+            _meshRenderer.material.color = chargeUpColors.Evaluate(chargeUpPercentage);
+
+            yield return null;
+        }
+
+        _lineRenderer.enabled = true;
+        _lineRenderer.SetPosition(0, firePoint.position);
+        _lineRenderer.SetPosition(1, player.transform.position);
+        yield return new WaitForSeconds(laserDuration);
+
+        Instantiate(explosionPrefab, firePoint.position, Quaternion.identity);
+
+        Destroy(gameObject);
+    } 
 }
