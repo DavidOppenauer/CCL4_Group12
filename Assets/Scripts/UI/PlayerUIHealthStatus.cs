de652b858dafbcfde6206ad31e7af55706d290ed
ViHoAT <vinh.ho-vn@hotmail.com>
2026-06-22 19:55:56 +0200

    Added Game Manager for Player Health throughout scenes

--- Assets/Scripts/UI/PlayerUIHealthStatus.cs
+++ Assets/Scripts/UI/PlayerUIHealthStatus.cs
@@ -5,19 +5,14 @@ public class PlayerUIHealthStatus : MonoBehaviour
 {
     [SerializeField] private GameObject player;
     [SerializeField] private Image healthBar;
-    private HealthSystem playerHealthSystem;
-    private float startHealth;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        playerHealthSystem = player.GetComponentInChildren<HealthSystem>();
-        startHealth = playerHealthSystem.GetCurrentHealth();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("player current health = " + playerHealthSystem.GetCurrentHealth() + "player max health = " + startHealth + "health percentage = " + playerHealthSystem.GetCurrentHealth() / startHealth);
-        healthBar.fillAmount = playerHealthSystem.GetCurrentHealth() / startHealth;
+        healthBar.fillAmount = GameManager.Instance.playerHealth / GameManager.Instance.playerMaxHealth;
     }
 }
