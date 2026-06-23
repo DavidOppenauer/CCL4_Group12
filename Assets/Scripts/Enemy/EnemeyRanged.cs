6da7f60a25e1a4ca374094f1cf380e7a7132b571
ViHoAT <vinh.ho-vn@hotmail.com>
2026-06-23 17:07:40 +0200

    Integrated Models and Animation for enemies also created new prefabs

--- Assets/Scripts/Enemy/EnemeyRanged.cs
+++ Assets/Scripts/Enemy/EnemeyRanged.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class EnemeyRanged : EnemyBase
 {
     [SerializeField] private Transform firePoint;
-    [SerializeField] private Gradient chargeUpColors;
+    // [SerializeField] private Gradient chargeUpColors;
     [SerializeField] private float chargeUpTime = 1.5f;
     [SerializeField] private float laserDuration = .5f;
     [SerializeField] private GameObject explosionPrefab;
     private LineRenderer _lineRenderer;
-    private MeshRenderer _meshRenderer;
+    // private MeshRenderer _meshRenderer;
     private float timer = 0f;
     private Coroutine activeCoroutine;
 
@@ -22,21 +22,24 @@ public class EnemeyRanged : EnemyBase
         base.Start();
         _lineRenderer = GetComponent<LineRenderer>();
         _lineRenderer.enabled = false;
-        _meshRenderer = GetComponentInChildren<MeshRenderer>();
+        // _meshRenderer = GetComponentInChildren<MeshRenderer>();
 
     }
 
     protected override void Attack()
     {
         if (activeCoroutine == null)
+        {
+            animator.SetTrigger("Charge");
             activeCoroutine = StartCoroutine(LaserSequence());
+        }
 
     }
 
     public override void OnHit()
     {
         healthSystem.TakeDamage(1);
-        timer = 0f;
+        StunEnemy();
         if (healthSystem.GetCurrentHealth() <= 0)
         {
             Destroy(gameObject);
@@ -50,12 +53,13 @@ public class EnemeyRanged : EnemyBase
         {
             timer += Time.deltaTime;
 
-            float chargeUpPercentage = timer / chargeUpTime;
+            // float chargeUpPercentage = timer / chargeUpTime;
 
-            _meshRenderer.material.color = chargeUpColors.Evaluate(chargeUpPercentage);
+            // _meshRenderer.material.color = chargeUpColors.Evaluate(chargeUpPercentage);
 
             yield return null;
         }
+        animator.SetTrigger("Attack");
         _lineRenderer.enabled = true;
         _lineRenderer.SetPosition(0, firePoint.position);
         _lineRenderer.SetPosition(1, player.transform.position);
@@ -65,5 +69,16 @@ public class EnemeyRanged : EnemyBase
         PlayerBrain playerBrain = player.GetComponent<PlayerBrain>();
         playerBrain.OnHit();
         Destroy(gameObject);
+    }
+    private void StunEnemy()
+    {
+        timer = 0f;
+        if (activeCoroutine != null)
+        {
+            StopCoroutine(activeCoroutine);
+            activeCoroutine = null;
+        }
+
+        animator.SetTrigger("Stun");
     }
 }
